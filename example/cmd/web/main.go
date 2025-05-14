e8b0607632004245dcbe0f6d7fbea4464fa52e9f
Asanbek09 <samudinua@gmail.com>
2025-05-14 13:03:57 +0600

    Реализация main.go

--- example/cmd/web/main.go
+++ example/cmd/web/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+
+const port = ":4000"
+
+type application struct {}
+
 func main() {
-	
+	//app := application{}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello design patterns")
+	})
+
+	fmt.Println("Starting web app on port", port)
+
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Panic(err)
+	}
 }
