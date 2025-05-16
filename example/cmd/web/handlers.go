f09dc87caef16d52a41955649bb498a11b41d887
Asanbek09 <samudinua@gmail.com>
2025-05-16 13:22:12 +0600

    Работа с render

--- example/cmd/web/handlers.go
+++ example/cmd/web/handlers.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"net/http"
-	"fmt"
 )
 
 func (app *application) ShowHome(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello design patterns")
+	app.render(w, "home.page.gohtml", nil)
 }
