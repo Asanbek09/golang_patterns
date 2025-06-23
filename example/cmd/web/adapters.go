1e92c58656217aa9604a0a686858f1c41a8006ee
Asanbek09 <samudinua@gmail.com>
2025-06-23 17:02:23 +0600

    Создаем адаптер для парсинга

---
+++ example/cmd/web/adapters.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"example/models"
+	"io"
+	"net/http"
+)
+
+type CatBreedsInterface interface {
+	GetAllCatBreeds() ([]*models.CatBreed, error)
+}
+
+type RemoteService struct {
+	Remote CatBreedsInterface
+}
+
+func (rs *RemoteService) GetAllBreeds() ([]*models.CatBreed, error) {
+	return rs.Remote.GetAllCatBreeds()
+}
+
+type JSONBackend struct {}
+
+func (jb *JSONBackend) GetAllCatBreeds() ([]*models.CatBreed, error) {
+	resp, err := http.Get("http://localhost:4000/api/cat-breeds/json")
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var breeds []*models.CatBreed
+	err = json.Unmarshal(body, &breeds)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return breeds, nil
+}
