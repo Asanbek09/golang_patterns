5ca97ef63359510af52dac0b54d6325d71353ee1
Asanbek09 <samudinua@gmail.com>
2025-06-25 18:06:11 +0600

    Небольшие изменения в wp/streamer

--- wp/streamer/pool.go
+++ wp/streamer/pool.go
@@ -6,3 +6,64 @@ type VideoDispatcher struct {
 	jobQueue chan VideoProcessingJob
 	Processor Processor
 }
+
+// type videoWorker
+type videoWorker struct {
+	id int
+	jobQueue chan VideoProcessingJob
+	workerPool chan chan VideoProcessingJob
+}
+
+// newVideoWorker
+func newVideoWorker(id int, workerPool chan chan VideoProcessingJob) videoWorker {
+	return videoWorker{
+		id: id,
+		jobQueue: make(chan VideoProcessingJob),
+		workerPool: workerPool,
+	}
+}
+
+// start starts a worker
+func (w videoWorker) start() {
+	go func() {
+		for {
+			// add job queue to the worker pool
+			w.workerPool <- w.jobQueue
+
+			// wait for a job to come back
+			job := <-w.jobQueue
+
+			// process the job
+			w.processVideoJob(job.Video)
+		}
+	}()
+}
+
+// run()
+func (vd *VideoDispatcher) Run() {
+	for i := 0; i < vd.maxWorkers; i++ {
+		worker := newVideoWorker(i+1, vd.WorkerPool)
+		worker.start()
+	}
+
+	go vd.dispatch()
+}
+
+// dispatch()
+func (vd *VideoDispatcher) dispatch() {
+	for {
+		// wait for a job to come in
+		job := <-vd.jobQueue
+
+		go func() {
+			workerJobQueue := <-vd.WorkerPool
+			workerJobQueue <- job
+		}()
+	}
+}
+
+// processVideoJob()
+
+func (w videoWorker) processVideoJob(video Video) {
+	video.encode()
+}
