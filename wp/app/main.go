673c2a153533a5329c4a4055c909f8fe4bf20641
Asanbek09 <samudinua@gmail.com>
2025-06-26 11:16:59 +0600

    Логируем некоторые части кода в wp

--- wp/app/main.go
+++ wp/app/main.go
@@ -7,8 +7,8 @@ import (
 
 func main() {
 	// define number of workers and jobs
-	const numJobs = 4
-	const numWorkers = 1
+	const numJobs = 1
+	const numWorkers = 2
 
 	// create channels for work and results
 	notifyChan := make(chan streamer.ProcessingMessage, numJobs)
@@ -19,11 +19,11 @@ func main() {
 
 	// Get a worker pool
 	wp := streamer.New(videoQueue, numWorkers)
-	fmt.Println("wp: ", wp)
 
 	// Start the worker pool
 	wp.Run()
-
+	fmt.Println("Worker pool started. Press enter to continue!")
+	_, _ = fmt.Scanln()
 	// Create a video to send to the worker pool
 	video := wp.NewVideo(1, "./input/puppy1.mp4", "./output", "mp4", notifyChan, nil)
 
@@ -31,5 +31,10 @@ func main() {
 	videoQueue <- streamer.VideoProcessingJob{Video: video}
 
 	// print out results
+	for i := 1; i <= numJobs; i++ {
+		msg := <-notifyChan
+		fmt.Println("i: ", i, "msg: ", msg)
+	}
 
+	fmt.Println("Done!")
 }
